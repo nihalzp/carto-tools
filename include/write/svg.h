f453587e5213231419bf1585e4b4a8165808158a
nihalzp <81457724+nihalzp@users.noreply.github.com>
2024-12-10 17:05:59 +0530

    Remove parameter names from svg.h

--- include/write/svg.h
+++ include/write/svg.h
@@ -10,21 +10,17 @@ private:
   int width, height;
 
 public:
-  SVG(const std::string &filename, int w, int h);
+  SVG(const std::string &, int, int);
 
   void add_segment(
-    double x1,
-    double y1,
-    double x2,
-    double y2,
-    const std::string &color = "black",
-    double stroke_width = 0.4);
+    double,
+    double,
+    double,
+    double,
+    const std::string & = "black",
+    double = 0.4);
 
-  void add_point(
-    double x,
-    double y,
-    const std::string &color = "red",
-    double radius = 0.9);
+  void add_point(double, double, const std::string & = "red", double = 0.9);
 
   ~SVG();
 };
