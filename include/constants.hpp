4d31f868cb31daabe57130ce79f329c66e699683
adisidev <64905594+adisidev@users.noreply.github.com>
2024-12-30 17:17:50 +0530

    Print `area_error` information to CSV

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,5 +2,5 @@
 
 #include <string>
 
-inline const std::string TARGET_AREA_COL_NAME = "Target area";
+inline const std::string TARGET_AREA_COL_NAME = "Target Area";
 const unsigned int NUM_PWH_TO_COMPARE = 1;
